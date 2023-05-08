7b33bfc1fa780ee68829f9d1a2ecd2a2aa807771
Jordan Bannister <40512499+Jordanb1997@users.noreply.github.com>
2023-05-08 12:33:08 +1000

    simple logic for performing operations

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let inputArray = [];
+
+let num1;
+let operator;
+let num2;
+
 function addition(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +18,16 @@ function multiplication(num1, num2) {
 
 function division(num1, num2) {
     return num1, num2;
+}
+
+function operate(num1, operator, num2) {
+    if (operator === '+') {
+        addition(num1, num2)
+    } else if (operator === '-') {
+        subtraction(num1, num2)
+    } else if (operator === '*') {
+        multiplication(num1, num2)
+    } else if (operator === '/') {
+        division(num1, num2)
+    }
 }
